79caca0e1e16de8df322f6b626a17048c20e9fed
Lavangislays <lavangisinghal@gmail.com>
2026-06-10 10:54:37 -0500

    my home work

--- python/fg.py
+++ python/fg.py
@@ -1,5 +1,19 @@
-# find average of the above subjects and write logic
+maths = 80
+science = 90
+english = 75
 
-# logic to give grade "A+" if avg >= 90
 
-# grade "A-" if >=85, B for >75 and < 85, rest "C"
+
+avg = (maths + science + english) / 3
+
+
+if avg >= 90:
+    grade = "A+"
+elif avg >= 85:
+    grade = "A-"
+elif 75 < avg < 85:
+    grade = "B"
+else:
+    grade = "C"
+
+print(f"Based on ypur score, your grade is : {grade}")
---
+++ python/home.py
@@ -0,0 +1,4 @@
+# Name 10 students as input , add it in list
+# sort it first so that names are alphabetwise
+# then print it with roll number
+# Tell me last 3 students names using indexing
